d17c99cea1b929535c99d52afcfe64028566a951
Martin Zachariassen <10747123+martinzachariassen@users.noreply.github.com>
2026-09-03 12:31:24 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { themeInit } from "./vite/theme-init";
+import { themeInit } from "./vite/theme-init.js";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), themeInit()],
